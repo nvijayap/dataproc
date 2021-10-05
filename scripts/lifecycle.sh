825f8b0435e774b566eb2adeb7a222940b004949
Naga Vijay <nvijayap@gmail.com>
2021-10-04 23:31:12 -0700

    time taken

--- scripts/lifecycle.sh
+++ scripts/lifecycle.sh
@@ -2,8 +2,16 @@
 
 cd `dirname $0`/..
 
+let S0=`date +%s`
+
 scripts/create_cluster.sh || exit $?
 
 scripts/submit_job.sh || exit $?
 
 scripts/delete_cluster.sh || exit $?
+
+let S1=`date +%s`
+
+let S=$S1-$S0
+
+echo -e "\nTime taken: $S seconds\n"
