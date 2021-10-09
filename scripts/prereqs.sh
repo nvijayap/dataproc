691e6bb313afa05a3b6da100a79c1d7e30e878c0
Naga Vijay <nvijayap@gmail.com>
2021-10-09 10:13:25 -0700

    image_version

--- scripts/prereqs.sh
+++ scripts/prereqs.sh
@@ -4,16 +4,15 @@
 # prereqs.sh
 # -----------
 
-OS=`uname -s`
+let ec=0
 
-if [ "$OS" == "Darwin" ]; then
-  :
-elif [ "$OS" == "Linux" ]; then
-  :
-else
-  echo -e "\nUnsupported OS - $OS\n"
-  exit 1
-fi
+exitnow() { let ec=$ec+1; exit $ec; }
+
+abort() { echo -e "\n . $1\n"; exitnow; }
+
+export OS=`uname -s`
+
+[ "$OS" != "Darwin" -a "$OS" != "Linux" ] && abort "Unsupported OS - $OS"
 
 cd `dirname $0`/..
 
@@ -25,65 +24,69 @@ if [ ! -f $config ]; then
 {
   "zone": "the-zone",
   "cluster": "dataproc-cluster-name",
+  "image_version": "dataproc-image-version",
   "keyfile": "/full/path/of/the-key-file"
 }
   "
-  exit 2
+  abort
 fi
 
-abort() { echo -e "\n . $1\n"; exit $2; }
+type brew >/dev/null 2>&1 || abort "Please install brew and then retry"
 
-type brew >/dev/null 2>&1 || abort "Please install brew and then retry" 3
-
-type jq >/dev/null 2>&1 || abort "Please 'brew install jq' and then retry" 4
+type jq >/dev/null 2>&1 || abort "Please 'brew install jq' and then retry"
 
 type sbt >/dev/null 2>&1
 
 if [ $? -ne 0 ]; then
   if [ "$OS" == "Darwin" ]; then
-    abort "Please 'brew install sbt' and then retry" 5
+    abort "Please 'brew install sbt' and then retry"
   else # Linux
-    abort "Please 'sdk install sbt' and then retry" 5
+    abort "Please 'sdk install sbt' and then retry"
   fi
 fi
 
-type terraform >/dev/null 2>&1 || abort "Please 'brew install terraform' and then retry" 6
+type terraform >/dev/null 2>&1 || abort "Please 'brew install terraform' and then retry"
 
-type gcloud >/dev/null 2>&1 || abort "Please install google-cloud-sdk and then retry" 7
+type gcloud >/dev/null 2>&1 || abort "Please install google-cloud-sdk and then retry"
 
-PROJECT=`gcloud config get-value project`
-
+export PROJECT=`gcloud config get-value project`
 if [ "$PROJECT" == "" ]; then
-  abort "Your gcloud config is not set properly; Unable to get the value for project" 8
+  abort "Your gcloud config is not set properly; Unable to get the value for project"
 else
   echo -e "\nPROJECT: $PROJECT"
 fi
 
-REGION=`gcloud config get-value dataproc/region`
-
+export REGION=`gcloud config get-value dataproc/region`
 if [ "$REGION" == "" ]; then
-  abort "Your gcloud config is not set properly; Unable to get the value for dataproc/region" 9
+  abort "Your gcloud config is not set properly; Unable to get the value for dataproc/region"
 else
   echo -e "\nREGION: $REGION"
 fi
 
-ZONE=`jq -r .zone $config`
+export ZONE=`jq -r .zone $config`
 if [ -z $ZONE -o "$ZONE" == "null" ]; then
-  abort "Your $config is not well-formed; it doesn't have the zone field" 10
+  abort "Your $config is not well-formed; it doesn't have the zone field"
 else
   echo -e "\nZONE: ${ZONE}"
 fi
 
-CLUSTER=`jq -r .cluster $config`
+export CLUSTER=`jq -r .cluster $config`
 if [ -z $CLUSTER -o "$CLUSTER" == "null" ]; then
-  abort "Your $config is not well-formed; it doesn't have the cluster field" 11
+  abort "Your $config is not well-formed; it doesn't have the cluster field"
 else
   echo -e "\nCLUSTER: ${CLUSTER}"
 fi
 
-KEYFILE=`jq -r '.keyfile' $config`
+export IMAGE_VERSION=`jq -r .image_version $config`
+if [ -z $IMAGE_VERSION -o "$IMAGE_VERSION" == "null" ]; then
+  abort "Your $config is not well-formed; it doesn't have the image_version field"
+else
+  echo -e "\nIMAGE_VERSION: ${IMAGE_VERSION}"
+fi
+
+export KEYFILE=`jq -r '.keyfile' $config`
 if [ -z $KEYFILE -o "$KEYFILE" == "null" ]; then
-  abort "Your $config is not well-formed; it doesn't have the keyfile field" 12
+  abort "Your $config is not well-formed; it doesn't have the keyfile field"
 else
   echo -e "\nKEYFILE: ${KEYFILE}"
 fi
